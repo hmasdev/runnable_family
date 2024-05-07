f939a4041c08c3f50a43dad3311cbca419481380
hmasdev <hmasuidev@gmail.com>
2024-05-07 22:09:22 +0900

    Add RunnableRandomBranch class to runnable_family/random.py and corresponding tests in tests/test_random.py

--- runnable_family/__init__.py
+++ runnable_family/__init__.py
@@ -1,6 +1,7 @@
 from .basic import RunnableConstant, RunnableAdd
 from .gacha import RunnableGacha
 from .loopback import RunnableLoopback
+from .random import RunnableRandomBranch
 from .runnable_diff import RunnableDiff
 from .self_consistent import RunnableSelfConsistent
 from .self_refine import RunnableSelfRefine
@@ -11,6 +12,7 @@ __all__ = [
     RunnableConstant.__name__,
     RunnableAdd.__name__,
     RunnableLoopback.__name__,
+    RunnableRandomBranch.__name__,
     RunnableGacha.__name__,
     RunnableDiff.__name__,
     RunnableSelfConsistent.__name__,
