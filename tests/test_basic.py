bd65664a061507768c1bd53027f85298b4254348
hmasdev <hmasuidev@gmail.com>
2024-06-01 22:01:51 +0900

    Add RunnablePartialLambda class

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,10 @@
 import pytest
+from typing import Callable
 from runnable_family.basic import (
     RunnableAdd,
     RunnableConstant,
     RunnableLog,
+    RunnablePartialLambda,
 )
 
 
@@ -49,6 +51,28 @@ def test_runnable_add_with_non_addable():
 
 
 @pytest.mark.parametrize(
+    'input_obj, func, kwargs, expected',
+    [
+        (0, lambda x: x+10, {}, 10),
+        ('x', lambda x, a: x + a, {'a': 'A'}, 'xA'),
+        ('z', lambda x, a: x + a, {'a': 'A'}, 'zA'),
+        ('x', lambda x, a, b: x + a + b, {'a': 'A', 'b': 'B'}, 'xAB'),
+    ]
+)
+def test_runnable_partial_lambda(
+    input_obj: str | int,
+    func: Callable[[str | int], str | int],
+    kwargs: dict[str, str | int],
+    expected: str | int,
+    mocker,
+) -> None:
+    mock_func = mocker.MagicMock(side_effect=func)
+    partial_lambda = RunnablePartialLambda(mock_func, **kwargs)
+    assert partial_lambda.invoke(input_obj) == expected
+    mock_func.assert_called_once_with(input_obj, **kwargs)
+
+
+@ pytest.mark.parametrize(
     'input_obj',
     [
         0,
