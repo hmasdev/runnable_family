40b6ffb71479d166129f928676daf21867160fa4
hmasdev <73353463+hmasdev@users.noreply.github.com>
2025-05-28 01:20:01 +0900

    refactor: update docstring for RunnableLog with clearer descriptions and examples

--- runnable_family/print_family.py
+++ runnable_family/print_family.py
@@ -7,12 +7,20 @@ T = TypeVar("T")
 
 
 class RunnableLog(RunnableLambda[T, T]):
-    '''Runnable that logs the input before returning it.
+    """Runnable that logs the input and returns it unchanged.
 
     Args:
-        output_func: Function to call with the input.
-            Default is logging.info.
-    '''
+        output_func: A callable that takes the input and logs it.
+            Defaults to `logging.info`.
+        **kwargs: Additional keyword arguments to pass to the base class.
+
+    Example:
+        >>> from runnable_family.print_family import RunnableLog
+        >>> log_runnable = RunnableLog()
+        >>> result = log_runnable.invoke("Hello, World!")
+        >>> # This will log "Hello, World!" using logging.info
+        >>> print(result)  # Output: "Hello, World!"
+    """
 
     def __init__(
         self,
@@ -20,13 +28,13 @@ class RunnableLog(RunnableLambda[T, T]):
         **kwargs,
     ):
         func = partial(
-            self.__identity_with_output,
+            self._identity_with_output,
             output_func=output_func
         )
         super().__init__(func, **kwargs)
 
     @staticmethod
-    def __identity_with_output(
+    def _identity_with_output(
         x: T,
         output_func: Callable[[T,], None] | None = None,
     ) -> T:
