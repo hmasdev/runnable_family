67b15f99aaa37332ab0ea0c639220ba714552659
hmasdev <73353463+hmasdev@users.noreply.github.com>
2025-05-28 02:34:49 +0900

    refactor: update RunnableDiff to inherit from RunnableSequence and enhance docstring

--- runnable_family/runnable_diff.py
+++ runnable_family/runnable_diff.py
@@ -2,6 +2,7 @@ from langchain_core.runnables import (
     Runnable,
     RunnableLambda,
     RunnableParallel,
+    RunnableSequence,
 )
 from langchain_core.runnables.base import Input, Output
 from typing import Any, Iterable, TypeVar
@@ -9,11 +10,36 @@ from typing import Any, Iterable, TypeVar
 InterMediate = TypeVar("InterMediate", covariant=True)
 
 
-class RunnableDiff(Runnable[Input, Output]):
-    ''' A simple implementation of Chain To Compare Two Runnables
-    '''
+class RunnableDiff(RunnableSequence[Input, Output]):
+    """
+    A runnable that computes the difference between the outputs of two
+    runnables. It runs two runnables in parallel and then applies a
+    diff function to their outputs.
 
-    _diff_chain: Runnable[Input, Output]
+    Args:
+        runnable1: The first runnable to run.
+        runnable2: The second runnable to run.
+        diff: A runnable or callable that takes two outputs and computes
+            the difference between them. It should accept an iterable of
+            two intermediate outputs and return the final output.
+    Example:
+        >>> from runnable_family.runnable_diff import RunnableDiff
+        >>> from langchain_core.runnables import RunnableLambda, RunnableParallel
+        >>> def runnable1(x):
+        ...     return x + 1
+        >>> def runnable2(x):
+        ...     return x + 2
+        >>> def diff(outputs):
+        ...     return outputs[0] - outputs[1]
+        >>> diff_runnable = RunnableDiff(
+        ...    runnable1=RunnableLambda(runnable1),
+        ...    runnable2=RunnableLambda(runnable2),
+        ...    diff=RunnableLambda(diff)
+        ... )
+        >>> result = diff_runnable.invoke(10)
+        >>> print(result)
+        -1
+    """
 
     def __init__(
         self,
@@ -21,32 +47,12 @@ class RunnableDiff(Runnable[Input, Output]):
         runnable2: Runnable[Input, InterMediate],
         diff: Runnable[Iterable[InterMediate], Output],
     ):
-        self._diff_chain = (
+        super().__init__(
             RunnableParallel(**{
                 'output1': runnable1,
                 'output2': runnable2,
-            })  # type: ignore
-            | RunnableLambda(dict.values)
-            | RunnableLambda(list)
-            | diff
+            }),
+            RunnableLambda(dict.values),
+            RunnableLambda(list),
+            diff,
         )
-
-    def invoke(self, *args, **kwargs) -> Output:
-        # To make it a concrete class
-        return self._diff_chain.invoke(*args, **kwargs)
-
-    def __getattribute__(self, name: str) -> Any:
-        if name in ["_diff_chain", "invoke"]:
-            return super().__getattribute__(name)
-        try:
-            return getattr(self._diff_chain, name)
-        except AttributeError:
-            return super().__getattribute__(name)
-
-    @property
-    def InputType(self) -> type[Input]:
-        return self._diff_chain.InputType
-
-    @property
-    def OutputType(self) -> type[Output]:
-        return self._diff_chain.OutputType
